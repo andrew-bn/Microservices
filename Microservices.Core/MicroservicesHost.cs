7b6d3c4fa902c3186ab55e718080a017e0511cc8
andrew-bn <mail.andrey.bulygin@gmail.com>
2015-11-17 20:45:58 +0200

    investigation

---
+++ Microservices.Core/MicroservicesHost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Core
+{
+    public abstract class Message
+    {
+
+    }
+    public interface MessageSource
+    {
+        Task<Message> Next();
+    }
+    public class MicroservicesHost
+    {
+        public MicroservicesHost()
+        {
+        }
+
+        
+    }
+}
